8fd158c6f80b06824a4de71aa4d58014082051a9
Simon <simonayotte17@gmail.com>
2023-11-07 08:51:27 -0500

    Streamlink wrapper

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+import Streamlink from './streamlink.js'
+
 console.log('Fetching stream...')
+
+let link = 'https://www.twitch.tv/zackrawrr'
+let stream = new Streamlink(link).output('./' + Date.now() + '.flv').start()
+
+stream.getQualities()
+
+stream.on('quality', (data) => {
+    console.log(data)
+})
+
+stream.on('err', (err) => {
+    console.log(err)
+})
+
+stream.on('end', (o) => {
+    console.log("Stream ended")
+    console.log(o)
+})
+
+stream.on('log', (data) => {
+    console.log(data)
+})
