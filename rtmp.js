5bbdc70d2d87880def73cf448675239af170d1c6
Simon <simonayotte17@gmail.com>
2023-11-08 16:31:56 -0500

    rtmp server

---
+++ rtmp.js
@@ -0,0 +1,25 @@
+import NodeMediaServer from "node-media-server";
+
+export default class RTMPServer {
+    constructor() {
+        const config = {
+            rtmp: {
+              port: 1935,
+              chunk_size: 60000,
+              gop_cache: true,
+              ping: 30,
+              ping_timeout: 60
+            },
+            http: {
+              port: 8000,
+              allow_origin: '*'
+            }
+        }
+
+        this.nms = new NodeMediaServer(config)
+    }
+
+    run() {
+        this.nms.run()
+    }
+}
